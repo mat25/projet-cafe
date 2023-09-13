97f6f2e7282212c9ded3518201ae5d23ae8995c2
mat25 <mateojean25660@gmail.com>
2023-09-13 15:39:31 +0200

    commit test merge

--- src/Fonctions/Facture_DonneRefVirementAttendu.php
+++ src/Fonctions/Facture_DonneRefVirementAttendu.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Fonctions;
 
+
     function Facture_DonneRefVirementAttendu($infoCommande, $infoentreprise): string
     {
         return "Brulerie_".$infoentreprise["numCompte"]."_".$infoCommande["id"];
